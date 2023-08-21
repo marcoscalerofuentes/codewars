e06ae3444c611bf52008663b73df5b33309c2c8c
marcoscalerofuentes <marcoscaleero@gmail.com>
2023-08-21 17:10:54 +0200

    nuevo ejercicio

---
+++ codewars/src/main/java/codewars/CreatePhone.java
@@ -0,0 +1,25 @@
+//Write a function that accepts an array of 10 integers (between 0 and 9), 
+//that returns a string of those numbers in the form of a phone number. "(123) 456-7890"
+
+public class Kata {
+  public static String createPhoneNumber(int[] numbers) {
+    String phone = "(";
+  for (int i = 0; i < numbers.length; i++) {
+    if(i<=2){
+      phone += numbers[i];
+    }
+     if(i==2){
+      phone += ") ";
+    }
+     if(i>2 && i<=5){
+      phone += numbers[i];
+    }
+    if(i==5){
+      phone += "-";
+    }
+    if(i>5){
+      phone += numbers[i];
+    }
+  }return phone;
+  }
+}
