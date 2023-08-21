adb8aa6038a74de9621afce6e8b817eee0da9f80
marcoscalerofuentes <marcoscaleero@gmail.com>
2023-08-21 16:44:24 +0200

    first commit

---
+++ codewars/src/main/java/codewars/ArrayPrinter.java
@@ -0,0 +1,18 @@
+package codewars;
+
+
+public class ArrayPrinter {
+public static void main(String[] args) {
+  Object[] array = new Object[] { 2, 4, 5, 2 };
+  System.out.println(printArray(array));
+}
+  public static String printArray(Object[] array) {
+    String exit = "";
+    for (int i = 0; i < array.length; i++) {
+      exit += array[i].toString();
+        if (i != array.length-1) {
+        exit += ",";}
+    }
+    return exit;
+  }
+}
